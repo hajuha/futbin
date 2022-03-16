77ee00a0f7b184471c1406ea46359c257b62a712
Hao Hoang <hajuha168@gmail.com>
2022-03-16 23:24:43 +0700

    fix

--- discord_bot.py
+++ discord_bot.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix='$', help_command=None)
 
 @bot.command()
 async def help(ctx):
-    message = "**Search FUT Card database:**\n---```\n$rn [nation] [position] [rating] [amount]\n```\nGet [*amount*] of [*nation*] [*position*] cards which rating below or equal to [*rating*]\nNation: eng, ger, fra, spa, bra, ita,...\n---```\n$rl [league] [position] [rating] [amount]\n```\nGet [*amount*] of [*league*] [*position*] cards which rating below or equal to [*rating*]\nLeague: eng1, fra1, Ger1, ITA1,...\n---```\n$rr [rating] [position] [amount]\n```\nGet [*amount*] [*position*] cards which rating below or equal to [*rating*]\n"
+    message = "**Search FUT Card database:**\nIf want to search all pos, position = 'ALL'\n---```\n$rn [nation] [position] [rating] [amount]\n```\nGet [*amount*] of [*nation*] [*position*] cards which rating below or equal to [*rating*]\nNation: eng, ger, fra, spa, bra, ita,...\n---```\n$rl [league] [position] [rating] [amount]\n```\nGet [*amount*] of [*league*] [*position*] cards which rating below or equal to [*rating*]\nLeague: eng1, fra1, Ger1, ITA1,...\n---```\n$rr [rating] [position] [amount]\n```\nGet [*amount*] [*position*] cards which rating below or equal to [*rating*]\n"
     await ctx.send(message)
 # @bot.command()
 # async def ovr(ctx, rating=100, league="", number=10):
@@ -44,21 +44,56 @@ async def help(ctx):
 #         memberlist = [f"```Top {league} {rating} cards:"] + memberlist
 #         await ctx.send("\n".join(memberlist)+"```")
 
-positions = {'GK', "CDM", "CM", "CB", "LB", "LWB", "RWB", "CAM",
+positions = {'ALL','GK', "CDM", "CM", "CB", "LB",'RB', "LWB", "RWB", "CAM",
              "RM", "LM", "CF", "ST", "RF", "LF", "RW", "LW",
              }
-
-
+nations = {'ENG': "14",
+        'FRA': "18",
+        "GER": "21",
+        "SPA": "45",
+        'NED': '34',
+        'POR': '38',
+        'ITA': '27',
+        'ARG': '52',
+        'BRA': '54',
+        'BEL': '7',
+        'DEN': '13',
+        'MEX': '83',
+        'URU': '60',
+        'CRO': '10',
+        'RUS': '40',
+        }
+MAX_PLAYER = 25
 def validate_pos(pos):
 
     return pos in positions
 
+def get_players(rating, league, country, position, number):
+    a = SoccerGuru()
+    player_dict = a.unload_pickle()
+    player_dict = a.filter(player_dict, rating, league, country, position)
+    overall_list = list(player_dict.keys())
+    overall_list.sort()
+    overall_list = reversed(overall_list)
+    player_list = []
+    count = 0
+    while count < number:
+        try:
+            overall = next(overall_list)
+            for j in range(0, len(player_dict[overall])):
+                player = player_dict[overall][j]
+                player_list.append(
+                    f"{str(overall)} {player[0]} {player[1]} {player[4]}")
+                count += 1
+        except Exception as e:
+            print(e)
+            break
+    return count, player_list
 
 @bot.command()
 async def rl(ctx, league="", position="", rating=99, number=10):
-    # await ctx.send("Updating...")
-    print('test')
     position = position.upper()
+    number = min(number, MAX_PLAYER)
     if not validate_pos(position):
         embed = discord.Embed()
         inner = '\n'.join(positions)
@@ -67,41 +102,25 @@ async def rl(ctx, league="", position="", rating=99, number=10):
 
         await ctx.send(embed=embed)
     else:
-        if position == "all":
+        if position == "ALL":
             position = ""
 
-        a = SoccerGuru()
         league = league.upper()
-        player_dict = a.unload_pickle()
-        player_dict = a.filter(player_dict, rating, league, "", position)
-        overall_list = list(player_dict.keys())
-        overall_list.sort()
-        overall_list = reversed(overall_list)
-        memberlist = []
-        count = 0
-        while count < min(number, 25):
-            try:
-                overall = next(overall_list)
-                for j in range(0, len(player_dict[overall])):
-                    player = player_dict[overall][j]
-                    memberlist.append(
-                        f"{str(overall)} {player[0]} {player[1]} {player[4]}")
-                    count += 1
-            except Exception as e:
-                print(e)
-                break
-        if len(memberlist) == 0:
+        count, player_list = get_players(rating, league, "", position, number)
+
+        if len(player_list) == 0:
             await ctx.send("No player found ! DM <@!333070457278562306> for more info")
         else:
-            memberlist = [
-                f"```\nTop {number} {league} {rating} cards: \n"] + memberlist
-            await ctx.send("\n".join(memberlist)+"```")
+            player_list = [
+                f"```\nTop {number} {league} {rating} cards: \n"] + player_list
+            await ctx.send("\n".join(player_list)+"```")
 
 
 @bot.command()
 async def rr(ctx, rating=99, position="", number=10):
-    # await ctx.send("Updating...")
+    number = min(number, MAX_PLAYER)
     position = position.upper()
+    
     if not validate_pos(position):
         embed = discord.Embed()
         inner = '\n'.join(positions)
@@ -110,39 +129,23 @@ async def rr(ctx, rating=99, position="", number=10):
 
         await ctx.send(embed=embed)
     else:
-        if position == "all":
+        if position == "ALL":
             position = ""
-        a = SoccerGuru()
-        player_dict = a.unload_pickle()
-        player_dict = a.filter(player_dict, rating, "", "", position)
-        overall_list = list(player_dict.keys())
-        overall_list.sort()
-        overall_list = reversed(overall_list)
-        memberlist = []
-        count = 0
-        while count < min(number, 25):
-            try:
-                overall = next(overall_list)
-                for j in range(0, len(player_dict[overall])):
-                    player = player_dict[overall][j]
-
-                    memberlist.append(
-                        f"{str(overall)} {player[2]} {player[0]} {player[1]} {player[4]}")
-                    count += 1
-            except Exception as e:
-                print(e)
-                break
-        if len(memberlist) == 0:
+            
+        count, player_list = get_players(rating, "", "", position, number)
+        if len(player_list) == 0:
 
             await ctx.send("No player found ! DM <@!333070457278562306> for more info")
         else:
-            memberlist = [f"```Top {number} {rating} cards: "] + memberlist
-            await ctx.send("\n".join(memberlist)+"```")
+            player_list = [f"```Top {number} {rating} cards: "] + player_list
+            await ctx.send("\n".join(player_list)+"```")
 
 
 @bot.command()
 async def rn(ctx, nation="ENG", position="", rating=99, number=10):
     position = position.upper()
+    number = min(number, MAX_PLAYER)
+    
     if not validate_pos(position):
         embed = discord.Embed()
         inner = '\n'.join(positions)
@@ -151,27 +154,12 @@ async def rn(ctx, nation="ENG", position="", rating=99, number=10):
 
         await ctx.send(embed=embed)
     else:
-        if position == "all":
+        if position == "ALL":
             position = ""
-    # await ctx.send("Updating...")
         nation = nation.upper()
-        nations = {'ENG': "14",
-                'FRA': "18",
-                "GER": "21",
-                "SPA": "45",
-                'NED': '34',
-                'POR': '38',
-                'ITA': '27',
-                'ARG': '52',
-                'BRA': '54',
-                'BEL': '7',
-                'DEN': '13',
-                'MEX': '83',
-                'URU': '60',
-                'CRO': '10',
-                'RUS': '40',
-                }
+
         nation_code = nations.get(nation, None)
+
         if nation_code is None:
             legit_nation = list(nations.keys())
             embed = discord.Embed()
@@ -181,32 +169,14 @@ async def rn(ctx, nation="ENG", position="", rating=99, number=10):
 
             await ctx.send(embed=embed)
         else:
-            a = SoccerGuru()
-            player_dict = a.unload_pickle()
-            player_dict = a.filter(player_dict, rating, "", nation_code, position)
-            overall_list = list(player_dict.keys())
-            overall_list.sort()
-            overall_list = reversed(overall_list)
-            memberlist = []
-            count = 0
-            while count < min(number, 25):
-                try:
-                    overall = next(overall_list)
-                    for j in range(0, len(player_dict[overall])):
-                        player = player_dict[overall][j]
-                        memberlist.append(
-                            f"{str(overall)} {player[2]} {player[0]} {player[1]} {player[4]}")
-                        count += 1
-                except Exception as e:
-                    print(e)
-                    break
-            if len(memberlist) == 0:
+            count, player_list = get_players(rating, "", nation_code, position, number)
+            if len(player_list) == 0:
 
                 await ctx.send("No player found ! DM <@!333070457278562306> for more info")
             else:
-                memberlist = [
-                    f"```Top {number} {nation} {rating} cards: "] + memberlist
-                await ctx.send("\n".join(memberlist)+"```")
+                player_list = [
+                    f"```Top {number} {nation} {rating} cards: "] + player_list
+                await ctx.send("\n".join(player_list)+"```")
 
 
 # @bot.command()
@@ -363,27 +333,6 @@ async def on_message(message):
                     if star(price) != 0:
                         await message.channel.send(f"{star(price)}")
                     await message.channel.send(f"<@!{message.author.id}> đã claim được `{player}` giá `{price}` lúc <t:{math.floor(datetime.timestamp(message.created_at))}:f>")
-
-                # if m1 and player != "Hãy":
-                #     price = int(m1.group().replace(",","").replace("`",""))
-                #     if price > price_alert:
-                #         await message.channel.send(f"`BIG CLAIM ALERT` <@&926665190974562354> <@!{message.author.id}> đã claim được `{player}` giá `{price}` lúc <t:{math.floor(datetime.timestamp(message.created_at))}:f>")
-                #     else:
-                #         await message.channel.send(f"<@!{message.author.id}> đã claim được `{player}` giá `{price}` lúc <t:{math.floor(datetime.timestamp(message.created_at))}:f>")
-                # else:
-                #     msg_2 = await bot.wait_for('message', check=check)
-                #     embed_msg_2 = msg_2.embeds[0]
-
-                #     m2 = re.search(PRICE_PATTERN, embed_msg_2.description)
-                #     player = embed_msg_2.title.split()[0]
-
-                #     if m2 and player != "Hãy":
-                #         price = int(m2.group().replace(",","").replace("`",""))
-                #         if price > 300000:
-                #             await message.channel.send(f"`BIG CLAIM ALERT` <@&926665190974562354> <@!{message.author.id}> đã claim được `{player}` giá `{price}` lúc <t:{math.floor(datetime.timestamp(message.created_at))}:f>")
-                #         else:
-                #             await message.channel.send(f"<@!{message.author.id}> đã claim được `{player}` giá `{price}` lúc <t:{math.floor(datetime.timestamp(message.created_at))}:f>")
-
     except Exception as e:
         print(e)
 
